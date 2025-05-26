87a5134a1933334dc239d9a4c52ed79ef70578d7
mike.makhovyk <m.makhovyk@gmail.com>
2025-05-26 23:04:18 +0300

    extract player ranking page to a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:tennis_court/player_ranking_page.dart';
 import 'player_model.dart';
 import 'player_service.dart';
 
@@ -26,83 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late Future<List<Player>> _playersFuture;
-  final PlayerService _playerService = PlayerService();
-
-  @override
-  void initState() {
-    super.initState();
-    _playersFuture = _playerService.getPlayers();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder<List<Player>>(
-              future: _playersFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No players found.'));
-                } else {
-                  final players = snapshot.data!;
-                  return ListView.builder(
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    itemCount: players.length,
-                    itemBuilder: (context, index) {
-                      return PlayerRow(player: players[index]);
-                    },
-                  );
-                }
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class PlayerRow extends StatelessWidget {
-  final Player player;
-
-  const PlayerRow({super.key, required this.player});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Text(
-        player.rank.toString(),
-        style: Theme.of(context).textTheme.titleMedium,
-      ),
-      title: Text(player.name),
-      subtitle: Text('Points: ${player.points}'),
-      trailing: Text(player.flag, style: const TextStyle(fontSize: 24)),
+      home: const PlayerRankingPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ lib/player_ranking_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:tennis_court/player_model.dart';
+import 'package:tennis_court/player_service.dart';
+
+class PlayerRankingPage extends StatefulWidget {
+  const PlayerRankingPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<PlayerRankingPage> createState() => _PlayerRankingPageState();
+}
+
+class _PlayerRankingPageState extends State<PlayerRankingPage> {
+  late Future<List<Player>> _playersFuture;
+  final PlayerService _playerService = PlayerService();
+
+  @override
+  void initState() {
+    super.initState();
+    _playersFuture = _playerService.getPlayers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder<List<Player>>(
+              future: _playersFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No players found.'));
+                } else {
+                  final players = snapshot.data!;
+                  return ListView.builder(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemCount: players.length,
+                    itemBuilder: (context, index) {
+                      return PlayerRow(player: players[index]);
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PlayerRow extends StatelessWidget {
+  final Player player;
+
+  const PlayerRow({super.key, required this.player});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text(
+        player.rank.toString(),
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+      title: Text(player.name),
+      subtitle: Text('Points: ${player.points}'),
+      trailing: Text(player.flag, style: const TextStyle(fontSize: 24)),
+    );
+  }
+}
