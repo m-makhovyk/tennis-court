0493704b894c16d4b4fd90cb46654a91b90ec6fb
mike.makhovyk <m.makhovyk@gmail.com>
2025-05-28 21:45:43 +0300

    change API endpoint

--- lib/player_model.dart
+++ lib/player_model.dart
@@ -1,9 +1,11 @@
 class Player {
-  final String id;
+  final int id;
   final String name;
   final int rank;
   final String countryAcr;
   final int points;
+  final int weeklyPoints;
+  final int weeklyPositionChange;
 
   Player({
     required this.id,
@@ -11,15 +13,19 @@ class Player {
     required this.rank,
     required this.countryAcr,
     required this.points,
+    required this.weeklyPoints,
+    required this.weeklyPositionChange,
   });
 
   factory Player.fromJson(Map<String, dynamic> json) {
     return Player(
-      id: json['player']['id'].toString(),
+      id: json['player']['id'],
       name: json['player']['name'],
       rank: json['position'],
       countryAcr: json['player']['countryAcr'],
-      points: json['point'],
+      points: json['pts'],
+      weeklyPoints: json['wkPts'],
+      weeklyPositionChange: json['wk'],
     );
   }
 }
--- lib/player_service.dart
+++ lib/player_service.dart
@@ -1,30 +1,24 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'player_model.dart';
 
 class PlayerService {
-  static const String _baseUrl =
-      'https://tennis-api-atp-wta-itf.p.rapidapi.com';
-  static String get _apiKey => dotenv.env['RAPIDAPI_KEY'] ?? '';
-  static const String _host = 'tennis-api-atp-wta-itf.p.rapidapi.com';
+  static const String _baseUrl = 'https://matchstat.com/tennis/api2';
 
   Future<List<Player>> getPlayers() async {
     final response = await http.get(
-      Uri.parse('$_baseUrl/tennis/v2/atp/ranking/singles/'),
-      headers: {'x-rapidapi-host': _host, 'x-rapidapi-key': _apiKey},
+      Uri.parse(
+        '$_baseUrl/ranking/atp/?date=26.05.2025&group=singles&page=0&includeAll=true',
+      ),
     );
 
     if (response.statusCode != 200) {
       throw Exception('API Error: ${response.statusCode} - ${response.body}');
     }
 
-    final Map<String, dynamic> jsonData = json.decode(response.body);
-    final List<dynamic> playersData = jsonData['data'];
+    final List<dynamic> jsonData = json.decode(response.body);
 
-    return playersData
-        .map((playerJson) => Player.fromJson(playerJson))
-        .toList();
+    return jsonData.map((playerJson) => Player.fromJson(playerJson)).toList();
   }
 }
